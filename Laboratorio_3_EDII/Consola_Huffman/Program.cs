5e915997d617e595627ffe4435e4869f50769586
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-10-21 20:55:51 -0600

    Correción de consola LZW
    Creando su proyecto respectivo

--- Laboratorio_3_EDII/Consola_Huffman/Program.cs
+++ Laboratorio_3_EDII/Consola_Huffman/Program.cs
@@ -9,7 +9,7 @@ namespace Consola_Huffman
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             Console.WriteLine("Ingrese el texto a comprimir");
             string Cadena = Console.ReadLine();
@@ -18,33 +18,6 @@ namespace Consola_Huffman
             Console.WriteLine("Cadena: " + Cadena);
             Console.WriteLine("Compresa Huffman: " + CompressHuffman.CompressionHuffman(Cadena));
             Console.ReadKey();
-
-            string RUTA = "/Users/eber.g/Desktop/PrimerAño/2Ciclo2020/EstructuraDatos II/Laboratorios/Laboratorio4_EDII/Laboratorio3_EDII/Laboratorio_3_EDII/Consola_Huffman/bin/Debug/netcoreapp3.1/Compress/Eber.lzw";
-
-
-
-
-
-            using(var File  = new FileStream(RUTA, FileMode.OpenOrCreate))
-            {
-                FileHandeling fileHandeling = new FileHandeling();
-                var extesión = Path.GetExtension(File.Name);
-
-                if(extesión == ".txt")
-                {
-                    fileHandeling.Create_File_Import();
-                    fileHandeling.Compress_LZW(File.Name, "Eber");
-
-                }
-                else if(extesión == ".lzw")
-                {
-                    fileHandeling.Create_File_Export();
-                    fileHandeling.Decompress_LZW(File.Name);
-                }
-
-            }
-            
-
 
         }
     }
---
+++ Laboratorio_3_EDII/Consola_LZW/Program.cs
@@ -0,0 +1,74 @@
+﻿using Laboratorio_3_EDII.Models;
+using System;
+using System.IO;
+
+namespace Consola_LZW
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\t..::LZW::..");
+                Console.WriteLine("Ingrese una opción:");
+                Console.WriteLine("  1)Compresión");
+                Console.WriteLine("  2)Descompresión");
+                Console.WriteLine("  3)Salir");
+                var option = Console.ReadLine();
+                var RUTA = string.Empty;
+                var newName = string.Empty;
+                FileHandeling fileHandeling = new FileHandeling();
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese la ruta del archivo a comprimir (extensión .txt)");
+                        RUTA = Console.ReadLine();
+                        Console.WriteLine("Ingrese el nombre del nuevo archivo");
+                        newName = Console.ReadLine();
+                        fileHandeling.Create_File_Import();
+                        using (var File = new FileStream(RUTA, FileMode.OpenOrCreate))
+                        {
+                            var ext = Path.GetExtension(File.Name);
+                            var new_Path = string.Empty;
+                            var old_Name = Path.GetFileName(File.Name);
+                            var path = Path.Combine($"Upload", old_Name);
+                            using (var this_file = new FileStream(path, FileMode.Create))
+                            {
+                                await File.CopyToAsync(this_file);
+                                new_Path = Path.GetFullPath(this_file.Name);
+                            }
+                            fileHandeling.Compress_LZW(new_Path, newName);
+                        }
+                        Console.WriteLine("Archivo compreso exitosamente!");
+                        break;
+                    case "2":
+                        Console.WriteLine("Ingrese la ruta del archivo a descomprimir (extensión .lzw)");
+                        RUTA = Console.ReadLine();
+                        fileHandeling.Create_File_Export();
+                        using (var File = new FileStream(RUTA, FileMode.OpenOrCreate))
+                        {
+                            var ext = Path.GetExtension(File.Name);
+                            var new_Path = string.Empty;
+                            var path = Path.Combine($"Upload", newName + ext);
+                            using (var this_file = new FileStream(path, FileMode.Create))
+                            {
+                                await File.CopyToAsync(this_file);
+                                new_Path = Path.GetFullPath(this_file.Name);
+                            }
+                            fileHandeling.Decompress_LZW(new_Path);
+                        }
+                        Console.WriteLine("Archivo descompreso exitosamente!");
+                        break;
+                    case "3":
+                        Console.WriteLine("Saliendo...");
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Por favor seleccione una opción válida.");
+                        break;
+                }
+            }
+        }
+    }
+}
